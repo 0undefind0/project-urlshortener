ab61ebe01f29751b18a52771d85658af99402fec
martin <steam.martinjuan@gmail.com>
2023-03-11 17:13:21 +0800

    initial commit for POST /api/shorturl endpoint

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const url = require('url'); 
+const dns = require('node:dns');
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded( { extended: false } ))
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -14,9 +18,42 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+
+
+// DATABASE
+
+
+
+// ROUTES
+
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+// todo: Save URL when URL is new
+// todo: Return short_url id if URL already exists
+app.post('/api/shorturl', function(req, res) {
+  const original_url = req.body.url;
+  let url_hostname = url.parse(original_url).hostname;
+  url_hostname = url_hostname.replace(/www./, ''); // remove the "www." at the beginning of the url
+
+  let response = {};
+
+  dns.lookup(url_hostname, function(err, address, family)  {
+    // Respond invalid url when Hostname is unreachable
+    if (err) {
+      response.error = 'invalid url'
+      res.json(response);
+      return;
+    };
+
+    response.original_url = original_url;
+    response.short_url = '??'
+    res.json(response);
+    return
+  });
+
 });
 
 app.listen(port, function() {
